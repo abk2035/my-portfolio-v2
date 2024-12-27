911d0ad15c9871a65375f7507276cb6b82eecd9c
abk237 <abk@gmail.com>
2024-12-27 08:10:30 +0100

    must fix a bug on slide-style.transform

--- js/script.js
+++ js/script.js
@@ -19,3 +19,48 @@
         resumeDetails[idx].classList.add('active');
     })
  })
+
+ const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+ const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+ let index = 0;
+
+ const activePortfolio = ()=>{
+    const imgSlide = document.getElementById('img-slide');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail');
+   
+    imgSlide.style.transform = `translateX(calc(${-index*100}%-${index*2}rem))`;
+    console.log('index'+index+'and'+imgSlide);
+
+    portfolioDetails.forEach(detail=>{
+        detail.classList.remove('active');
+    })
+    portfolioDetails[index].classList.add('active');
+
+}
+
+arrowRight.addEventListener('click',()=>{
+    if(index<2){
+        index++;
+        arrowLeft.classList.remove('disabled');
+
+    }else{
+        index=3;
+        arrowRight.classList.add('disabled');
+    }
+
+    activePortfolio();
+    console.log('herer'+index);
+})
+
+arrowLeft.addEventListener('click',()=>{
+    if(index>1){
+        index--;
+        arrowRight.classList.remove('disabled');
+    }else{
+        index=0;
+        arrowLeft.classList.add('disabled');
+    }
+
+    activePortfolio();
+})
